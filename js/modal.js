ab2d42223cdfbba9a799bc4035626a1434ba6aab
gisele-araujo <gisele.flor@bandtec.com.br>
2020-05-15 14:48:37 -0300

    organização js

---
+++ js/modal.js
@@ -0,0 +1,30 @@
+// MODAL 
+function iniciaModal (modalID) { //função que chama o modal com parametro do id do modal
+    const modal = document.getElementById (modalID); // pegue o elemento modalID
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (event) => {
+        if(event.target.id == modalID || event.target.className == 'fechar'){
+            modal.classList.remove ('mostrar');
+        }
+    })
+}
+
+// login
+const logar_menu = document.getElementById ('login_acesso'); 
+const logar_rodape = document.getElementById ('login_rodape');
+logar_menu.addEventListener('click', function() {
+    iniciaModal ('modal-login') 
+})
+logar_rodape.addEventListener('click', function() {
+    iniciaModal ('modal-login')
+})
+
+// cadastro
+const cadastrar_menu = document.getElementById ('cadastrar_acesso');
+const cadastrar_rodape = document.getElementById ('cadastro_rodape');
+cadastrar_menu.addEventListener ('click', function() {
+    iniciaModal ('modal-cadastro')
+})
+cadastrar_rodape.addEventListener ('click', function() {
+    iniciaModal ('modal-cadastro')
+})
