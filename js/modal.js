ab089af690483faa4cf2d3781cbddd1d9913282a
gisele-araujo <gisele.flor@bandtec.com.br>
2020-05-15 19:24:14 -0300

    melhorias

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,19 @@
 // MODAL 
 function iniciaModal (modalID) { 
     const modal = document.getElementById (modalID); 
-    modal.classList.add('mostrar');
+    modal.classList.add('mostrar'); //ativar classe .mostrar 
     modal.addEventListener('click', (event) => {
         if(event.target.id == modalID || event.target.className == 'fechar'){
-            modal.classList.remove ('mostrar');
+            modal.classList.remove ('mostrar'); //desativar classe .mostrar 
         }
     })
 }
 
 // login
-const logar_menu = document.getElementById ('login_acesso'); 
+const logar_menu = document.getElementById ('login_acesso'); // id do elemento clicável 
 const logar_rodape = document.getElementById ('login_rodape');
 logar_menu.addEventListener('click', function() {
-    iniciaModal ('modal-login') 
+    iniciaModal ('modal-login') // id do modal
 })
 logar_rodape.addEventListener('click', function() {
     iniciaModal ('modal-login')
